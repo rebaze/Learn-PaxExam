e7d1d5be159faec91535201b7533add71aa655fd
Toni Menzel <toni@okidokiteam.com>
2011-05-19 19:07:30 +0200

    cleanup

--- lesson-cxf/src/test/java/org/ops4j/pax/exam/learn/cxf/CXFLaboratories.java
+++ lesson-cxf/src/test/java/org/ops4j/pax/exam/learn/cxf/CXFLaboratories.java
@@ -1,70 +1,48 @@
 package org.ops4j.pax.exam.learn.cxf;
 
-import static org.ops4j.pax.exam.LibraryOptions.easyMockBundles;
-import static org.ops4j.pax.exam.LibraryOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.OptionUtils.*;
-import static org.ops4j.pax.tinybundles.core.TinyBundles.*;
-import org.ops4j.pax.tinybundles.core.TinyBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.provision;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
+import static org.ops4j.pax.tinybundles.core.TinyBundles.withBnd;
 
-import static org.ops4j.pax.exam.spi.container.PaxExamRuntime.getTestContainerFactory;
-
-import java.io.IOException;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.TestAddress;
 import org.ops4j.pax.exam.TestContainer;
 import org.ops4j.pax.exam.TestProbeBuilder;
-import org.ops4j.pax.exam.TestProbeProvider;
 import org.ops4j.pax.exam.apart.DoSomething;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.container.PaxExamRuntime;
-import org.ops4j.pax.exam.spi.container.PlumbingContext;
 import org.ops4j.pax.exam.testforge.BundlesInState;
 import org.ops4j.pax.exam.testforge.SingleClassProvider;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleException;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Here's a somewhat different test setup that is made to elaborate on foreign
  * technology to be tested on a given target framework. So here we do care on
  * 
+ * - the set of bundles loaded
+ * - single target framework
+ *  - not really about the probe but on being able to interactively "play" with the framework (shell)
+ * - Creating companion bundles "at will" using tinybundles.
  * 
- * -- the set of bundles loaded
- * 
- * -- single target framework
- * 
- * -- not really about the probe but on being able to interactively "play" with
- * the framework (shell)
- * 
- * -- Creating companion bundles "at will" using tinybundles.
- * 
- * @author tonit
+ * @author Toni Menzel
  * 
  */
 @RunWith( JUnit4TestRunner.class )
 public class CXFLaboratories {
-	
-	private static Logger LOG = LoggerFactory.getLogger(CXFLaboratories.class.getName());
 	
 	@Configuration
 	public Option[] configure() {
 		return options(
 				systemProperty( "org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
 				
-				
-				//mavenBundle().groupId("org.apache.aries.blueprint").artifactId("org.apache.aries.blueprint").version("0.3"),
-				//mavenBundle().groupId("org.apache.aries.proxy").artifactId("org.apache.aries.proxy").version("0.3"),
-				//mavenBundle().groupId("org.apache.aries").artifactId("org.apache.aries.util").version("0.3"),
 				mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-jetty-bundle").version("1.0.3"),
 				mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.asm").version("3.3_2"),
 				
@@ -100,20 +78,9 @@ public class CXFLaboratories {
 	}
 	
 	@Test
-	public void runTest( BundleContext ctx )  {
-		
-	}
-	
-	//@Test
 	public TestAddress saneLoggerFactory( TestProbeBuilder builder )
 	{
        return builder.addTest( SingleClassProvider.class, (Object)LoggerFactory.class.getName() );
-	}
-	
-	@Test
-	public TestAddress sane( TestProbeBuilder builder )
-	{
-       return builder.addTest( SingleClassProvider.class, (Object)BundleActivator.class.getName() );
 	}
 	
 	@Test
